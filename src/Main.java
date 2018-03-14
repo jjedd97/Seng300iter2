d12bdce223b3ad5c992ddeb33c009b15ab258406
bobbyharley <37085542+bobbyharley@users.noreply.github.com>
2018-03-13 21:42:24 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,33 @@
-import java.io.BufferedReader;
+package typeSearch;
+
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.*;
- 
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
- 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
- 
-	//use ASTParse to parse string
-	public static void parse(String str) {
-		ASTParser parser = ASTParser.newParser(AST.JLS8);
-		parser.setSource(str.toCharArray());
-		parser.setKind(ASTParser.K_COMPILATION_UNIT);
- 
-		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
- 
-		cu.accept(new ASTVisitor() {
- 
-			Set names = new HashSet();
- 
-			public boolean visit(VariableDeclarationFragment node) {
-				SimpleName name = node.getName();
-				this.names.add(name.getIdentifier());
-				System.out.println("Declaration of '" + name + "' at line"
-						+ cu.getLineNumber(name.getStartPosition()));
-				return false; // do not continue 
+	
+	public static File[] path;
+	public static String type;
+	public static int declarationsFound;
+	public static int referencesFound;
+
+	public static void main(String[] args) {
+		Program program = new Program(args[0], args[1]);
+
+		// Code taken from: http://adam-bien.com/roller/abien/entry/java_8_reading_a_file
+		for (File getJavaFile:Program.path) {
+			String fileToString;
+
+			try {
+				fileToString = new String(Files.readAllBytes(Paths.get(getJavaFile.toString())));
+			} catch (IOException ioe) {
+				continue;
 			}
- 
-			public boolean visit(SimpleName node) {
-				if (this.names.contains(node.getIdentifier())) {
-					//System.out.println("Usage of '" + node + "' at line "
-					//		+ cu.getLineNumber(node.getStartPosition()));
-				}
-				return true;
-			}
-		});
- 
-	}
- 
-	//read file content into a string
-	public static String readFileToString(String filePath) throws IOException {
-		StringBuilder fileData = new StringBuilder(1000);
-		BufferedReader reader = new BufferedReader(new FileReader(filePath));
- 
-		char[] buf = new char[10];
-		int numRead = 0;
-		while ((numRead = reader.read(buf)) != -1) {
-			System.out.print("Enter a directory: ");
-			Scanner scanner = new Scanner(System.in);
-			String directory = scanner.nextLine();
-			
-			System.out.print("Enter a Java type: ");
-			Scanner scanner2 = new Scanner(System.in);
-			String type = scanner2.nextLine();
-			
-			System.out.println("<" + type + ">. Declarations found: <" + numRead + ">" + "; references found: <" + numRead + ">.");
-			
-			//System.out.println(numRead);
-			String readData = String.valueOf(buf, 0, numRead);
-			fileData.append(readData);
-			buf = new char[1024];
+
+			program.parse(fileToString);
 		}
- 
-		reader.close();
- 
-		return  fileData.toString();	
-	}
- 
-	//loop directory to get file list
-	public static void ParseFilesInDir() throws IOException{
-		File dirs = new File(".");
-		String dirPath = dirs.getCanonicalPath() + File.separator+"src"+File.separator;
- 
-		File root = new File(dirPath);
-		//System.out.println(root.listFiles());
-		File[] files = root.listFiles ( );
-		String filePath = null;
- 
-		 for (File f : files ) {
-			 filePath = f.getAbsolutePath();
-			 if(f.isFile()){
-				 parse(readFileToString(filePath));
-			 }
-		 }
-	}
- 
-	public static void main(String[] args) throws IOException {
-		ParseFilesInDir();
+
+		System.out.println(type + ". Declarations found: " + declarationsFound + "; references found: " + referencesFound + ".");
 	}
 }
