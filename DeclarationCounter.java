8ef344937a296848ef4efbfb4aa5b9e92d0bd1e7
Jocelyn <36179505+jjedd97@users.noreply.github.com>
2018-03-23 21:24:46 -0600

    fixed a thing

--- DeclarationCounter.java
+++ DeclarationCounter.java
@@ -1,5 +1,7 @@
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 public class DeclarationCounter {
@@ -15,8 +17,25 @@ public class DeclarationCounter {
 		cu.accept(new ASTVisitor() { //create visitor for TypeDeclaration
 			// Count Declarations
 			public boolean visit(TypeDeclaration node) {
-				node.resolveBinding();
+				ITypeBinding bind=node.resolveBinding();
 				String qualifiedName = node.getName().getFullyQualifiedName(); //get node name
+				if (bind!=null)
+				{
+					qualifiedName=bind.getQualifiedName();
+				}
+				if (type.equals(qualifiedName)) { //compare
+					count++; //if equal update counter
+				}
+				return true;
+			}
+		});
+		
+		cu.accept(new ASTVisitor() { //create visitor for TypeDeclaration
+			// Count Declarations
+			public boolean visit(AnonymousClassDeclaration node) {
+				ITypeBinding bind =node.resolveBinding();
+	
+				String qualifiedName = bind.getQualifiedName();//get node name
 				if (type.equals(qualifiedName)) { //compare
 					count++; //if equal update counter
 				}
